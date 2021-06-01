c99961440472a470a7d24be72f7aafc5602e7d26
jyotichahal <jyotisukhija@gmail.com>
2021-06-01 22:28:01 +1000

    did few changes based on the requirements

--- mindarc-exercise-2-jyoti/src/app/app.component.ts
+++ mindarc-exercise-2-jyoti/src/app/app.component.ts
@@ -1,25 +1,36 @@
 import { Component, ViewChild } from '@angular/core';
 import {
-  trigger,
-  state,
-  style,
-  animate,
-  transition
+    trigger,
+    state,
+    style,
+    animate,
+    transition
 } from '@angular/animations';
 import dataDetails from './data.json';
 
 interface Data {
-  title: String;
-  content: String;
+    title: String;
+    content: String;
 }
 
 @Component({
-  selector: 'my-app',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'my-app',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  name = 'Angular';
+    name = 'Angular';
 
-  dataDisplay: Data[] = dataDetails;
+    dataDisplay: Data[] = dataDetails;
+
+    lastId = 'tab0';
+    toggleTabs(event: { target: { id: string } }) {
+        if (this.lastId === event.target.id) {
+            this.lastId = 'tabgenesis';
+            const ele = document.getElementById('tabgenesis') as HTMLInputElement;
+            ele.checked = true;
+        } else {
+            this.lastId = event.target.id;
+        }
+    }
 }
