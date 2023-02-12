bb28879c91311d241fb25c04e8dfc47bd36f061e
keithpaul <keith.paul@okendo.io>
2023-02-12 12:23:23 +1100

    Add bootstrap to project and test with button

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,16 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <HomeView />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HomeView from '@/views/HomeView.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HomeView
   }
 }
 </script>
