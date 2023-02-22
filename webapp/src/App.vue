5bc082b7002a8768b5d57d6f6e48450f8bc51fe2
IK Mendoza <iken.mndz@gmail.com>
2023-02-22 18:21:01 +0800

    created vue js app

---
+++ webapp/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <TopBanner />
+</template>
+
+<script>
+import TopBanner from "./components/TopBanner.vue";
+
+export default {
+  name: "App",
+  components: {
+    TopBanner,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
