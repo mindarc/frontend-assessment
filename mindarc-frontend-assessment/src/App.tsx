522126cac2c34ccafb2e93ff5e8f9b405a41c4ab
danriley11 <dan_riley7@outlook.com>
2023-12-12 18:14:51 +1100

    temp update on App for easier testing

--- mindarc-frontend-assessment/src/App.tsx
+++ mindarc-frontend-assessment/src/App.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import ContentTile from './components/contentTile/contentTile';
 import DataReading from './components/dataReading/dataReading';
@@ -5,6 +6,19 @@ import Header from './components/header/header';
 
 function App() {
   const contentTileArray = [1, 2, 3];
+  const [isMobile, setIsMobile] = useState(window.innerWidth <= 600);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsMobile(window.innerWidth <= 600);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, [isMobile]);
 
   return (
     <>
@@ -12,14 +26,14 @@ function App() {
         <Header />
       </header>
 
+      <section className="data-reading">
+        <DataReading isMobile={isMobile} />
+      </section>
+
       <section className="content-tiles">
         {contentTileArray.map((_, i) => (
           <ContentTile key={i} />
         ))}
-      </section>
-
-      <section className="data-reading">
-        <DataReading />
       </section>
     </>
   );
