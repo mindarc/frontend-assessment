f10fa5904af2b4be66b3da3ef4fc28ee566d3eb5
danriley11 <dan_riley7@outlook.com>
2023-12-12 17:00:56 +1100

    cleanup App.tsx/.css

--- mindarc-frontend-assessment/src/App.tsx
+++ mindarc-frontend-assessment/src/App.tsx
@@ -1,7 +1,6 @@
-// import reactLogo from './assets/react.svg';
-// import viteLogo from '/vite.svg';
 import './App.css';
 import ContentTile from './components/contentTile/contentTile';
+import DataReading from './components/dataReading/dataReading';
 import Header from './components/header/header';
 
 function App() {
@@ -13,11 +12,15 @@ function App() {
         <Header />
       </header>
 
-      <body className="body">
+      <section className="content-tiles">
         {contentTileArray.map((_, i) => (
           <ContentTile key={i} />
         ))}
-      </body>
+      </section>
+
+      <section className="data-reading">
+        <DataReading />
+      </section>
     </>
   );
 }
