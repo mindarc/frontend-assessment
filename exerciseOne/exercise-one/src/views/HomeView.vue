b6892458c7688f16e69fe6bbe4a8e104597b3045
patricia <59278560+Patriciapavia@users.noreply.github.com>
2023-12-15 14:48:39 +1100

    initial commit

---
+++ exerciseOne/exercise-one/src/views/HomeView.vue
@@ -0,0 +1,20 @@
+<template>
+    <h1>Home</h1>
+
+  </template>
+  <script>
+  import { defineComponent } from 'vue'
+
+  export default defineComponent({
+    name: 'HomeView'
+  })
+  </script>
+
+  <script setup>
+
+  </script>
+
+  <style scoped>
+
+
+  </style>
