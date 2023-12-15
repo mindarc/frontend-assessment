0ea971527201aed6755e72eb6a3147b25a93bc0c
patricia <59278560+Patriciapavia@users.noreply.github.com>
2023-12-15 14:56:55 +1100

    setting up bootstrap

--- exerciseOne/exercise-one/src/views/HomeView.vue
+++ exerciseOne/exercise-one/src/views/HomeView.vue
@@ -1,6 +1,10 @@
 <template>
     <h1>Home</h1>
-
+    <div class="container">
+     <button type="button" class="btn btn-success">Success</button>
+     <button type="button" class="btn btn-info">Info</button>
+     <button type="button" class="btn btn-warning">Warning</button>
+  </div>
   </template>
   <script>
   import { defineComponent } from 'vue'
