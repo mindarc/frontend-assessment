00cc0d9af2efd723c987731f8a0dad018e16b40e
patricia <59278560+Patriciapavia@users.noreply.github.com>
2023-12-15 20:45:19 +1100

    create hero component

---
+++ exerciseOne/exercise-one/src/components/heroComponent.vue
@@ -0,0 +1,40 @@
+
+
+<template>
+  <div class="hero">
+  <div>
+    <div class="d-flex justify-content-center align-items-center">
+      <div class="text-white">
+        <h1 >{{ heading }}</h1>
+        <p>{{ subheading }}</p>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script setup>
+defineProps({
+  heading: {
+    type: String,
+    required: true
+    },
+    subheading: {
+      type: String,
+      required: true
+    }
+})
+</script>
+
+<style scoped>
+.hero {
+  background-image: url('../assets/1920x450.png'); /* Path to your local image file */
+  background-position: center;
+  height: 40vh; /* S300pxet the height as needed */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  color: white;
+}
+</style>
