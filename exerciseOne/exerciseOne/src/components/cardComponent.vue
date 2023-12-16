45a89678e7edba932e3283ccc39d8e1bfabfc962
patricia <59278560+Patriciapavia@users.noreply.github.com>
2023-12-16 14:48:12 +1100

    update

--- exerciseOne/exerciseOne/src/components/cardComponent.vue
+++ exerciseOne/exerciseOne/src/components/cardComponent.vue
@@ -1,15 +1,15 @@
 <template>
-  <div class="container py-5">
-    <div class="row pb-5 mb-4">
+  <div class="container py-5 text-center">
+    <div class="row pb-5 mb-4 ">
       <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
-        <div class="card rounded shadow-sm border-0">
+        <div class="card rounded shadow-sm border-1">
           <div class="card-body p-4">
             <img src="../assets/400x300.png" alt="" class="img-fluid d-block mx-auto mb-3" />
-            <p class="small text-muted font-italic">
+            <p class="small text-muted">
               {{ description }}
             </p>
             <div class="text-center">
-              <button class="rounded-pill border-2 px-3">
+              <button class="rounded-pill border-2 px-3 bg-white text-dark border-black">
                 {{ actionText }}
               </button>
             </div>
@@ -18,14 +18,14 @@
       </div>
       <!-- card 2 -->
       <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
-        <div class="card rounded shadow-sm border-0">
+        <div class="card rounded shadow-sm border-1">
           <div class="card-body p-4">
             <img src="../assets/400x300.png" alt="" class="img-fluid d-block mx-auto mb-3" />
-            <p class="small text-muted font-italic">
+            <p class="small text-muted">
               {{ description }}
             </p>
             <div class="text-center">
-              <button class="rounded-pill border-2">
+              <button class="rounded-pill border-2 px-3 bg-white text-dark border-black">
                 {{ actionText }}
               </button>
             </div>
@@ -34,14 +34,14 @@
       </div>
       <!-- card 3 -->
       <div class="col-lg-4 col-md-6 mb-4 mb-lg-0">
-        <div class="card rounded shadow-sm border-0">
+        <div class="card rounded shadow-sm border-1">
           <div class="card-body p-4">
             <img src="../assets/400x300.png" alt="" class="img-fluid d-block mx-auto mb-3" />
-            <p class="small text-muted font-italic">
+            <p class="fw-normal">
               {{ description }}
             </p>
             <div class="text-center">
-              <button class="rounded-pill border-2">
+              <button class="rounded-pill border-2 px-3 bg-white text-dark border-black">
                 {{ actionText }}
               </button>
             </div>
@@ -60,8 +60,6 @@ export default {
   },
   methods: {
     myMethod() {
-      // Log something inside a method
-      console.log('Logging from myMethod');
     }
   },
   mounted() {
--- exerciseOne/exerciseOne/src/components/heroComponent.vue
+++ exerciseOne/exerciseOne/src/components/heroComponent.vue
@@ -3,10 +3,10 @@
 <template>
   <div class="hero">
   <div>
-    <div class="d-flex justify-content-center align-items-center">
+    <div class="d-flex justify-content-center align-items-center text-center">
       <div class="text-white">
-        <h1 >{{ heading }}</h1>
-        <p>{{ subheading }}</p>
+        <h1 class="text-bold">{{ heading }}</h1>
+        <h6>{{ subheading }}</h6>
       </div>
     </div>
   </div>
@@ -30,7 +30,8 @@ defineProps({
 .hero {
   background-image: url('../assets/1920x450.png'); /* Path to your local image file */
   background-position: center;
-  height: 40vh; /* S300pxet the height as needed */
+  background-size: cover;
+  height: 45vh; /* S300pxet the height as needed */
   display: flex;
   justify-content: center;
   align-items: center;
