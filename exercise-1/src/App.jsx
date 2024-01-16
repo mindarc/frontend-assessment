ef38fbb33804a90649eb1ec758fc4819b01a614c
Ray Rillon <ray.rillon@convertdigital.com.au>
2024-01-16 22:41:00 +0800

    add hero component

--- exercise-1/src/App.jsx
+++ exercise-1/src/App.jsx
@@ -1,5 +1,17 @@
+import { Hero } from './components/Hero'
+
 function App() {
-  return <></>
+  return (
+    <main>
+      <Hero
+        desktopImage="https://via.placeholder.com/1920x650"
+        mobileImage="https://via.placeholder.com/600x600"
+        title="Hello Developer!"
+        description="Lorem, ipsum dolor sit amet consectetur adipisicing elit"
+        className="text-white"
+      />
+    </main>
+  )
 }
 
 export default App
