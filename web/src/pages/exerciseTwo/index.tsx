0f3a16785fc89f84fffc1b1629d2a81fa904174b
basinghse <bsinghv8@gmail.com>
2024-08-25 18:52:27 +1000

    test tab/accordion

--- web/src/pages/exerciseTwo/index.tsx
+++ web/src/pages/exerciseTwo/index.tsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { TabAccordion } from "../../components/TabAccordion";
+import data from "../../data.json";
+
+interface ContentItem {
+  id: number;
+  title: string;
+  content: string;
+}
 
 export const ExerciseTwo: React.FC = () => {
+  const [items, setItems] = useState<ContentItem[]>([]);
+
+  useEffect(() => {
+    setItems(data);
+  }, []);
+
   return (
-    <div>
-      <h1>Exercise Two</h1>
+    <div className="min-h-screen bg-white pt-16">
+      <div className="container mx-auto px-4 py-8">
+        <TabAccordion items={items} />
+      </div>
     </div>
   );
 };
