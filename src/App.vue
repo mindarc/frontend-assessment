f4102eaee44a5e028ce0456e5e16936dcfad325b
icyms <icyjms@yahoo.com>
2024-08-28 10:34:21 +0800

    Implementation of exercise 1 frontend assessment

---
+++ src/App.vue
@@ -0,0 +1,137 @@
+<template>
+  <div class="header">
+    <img v-if="!isMobile" class="image-container" alt="Vue logo" src="https://via.placeholder.com/1920x650">
+    <img v-if="isMobile" class="image-container" alt="Vue logo" src="https://via.placeholder.com/600x600">
+    <div class="header-text-container">
+      <p class="header-text-greeting">Hello Developer!</p>
+      <h3 class="header-text-centered">Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+    </div>
+    <p class="header-text-bottom-right">Powered by HTML.COM</p>
+  </div>
+  <CardList />
+</template> 
+
+<script>
+import CardList from './components/CardList.vue'
+
+export default {
+  name: 'App',
+  components: {
+    CardList
+  },
+  computed: {
+    isMobile() {
+      if (screen.width <= 760) {
+          return true
+        } else {
+          return false
+        }
+    } 
+  }
+}
+</script>
+
+<style>
+* { 
+ margin: 0; 
+ padding: 0; 
+ box-sizing: border-box; 
+} 
+
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+
+#app ul {
+  list-style: none;
+}
+
+#app li {
+  text-align: center;
+}
+
+#app button {
+  cursor: pointer;
+  border: 3px solid rgb(68, 68, 68);
+  color: rgb(68, 68, 68);
+  padding: 8px;
+  margin: 5px;
+  border-radius: 18px;
+  /* width: 45%; */
+  font-weight: 800;
+  font-size: 12px;
+  font-family: Arial, Helvetica, sans-serif;
+  letter-spacing: 2px;
+}
+
+body {
+  overflow-x: hidden; /* Hide horizontal scrollbar */
+}
+
+.header{
+  position: relative;
+  text-align: center;
+  color: white;
+  display: flex;
+  justify-content: center;
+}
+
+.header-text-container {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform:translate(-50%, -50%);
+}
+
+.header-text-greeting {
+  font-size: 50px;
+  font-weight: bold;
+}
+
+.header-text-centered {
+  font-weight: 100;
+  font-size: 28px;
+}
+
+.header-text-bottom-right {
+  position: absolute;
+  bottom: 5px;
+  right: 5px;
+  font-size: 8px;
+  color: rgb(112, 112, 112);
+}
+
+.image-container {
+  width: 100vw;
+}
+
+ @media only screen and (max-width: 700px) {
+
+    .header-text-greeting {
+      font-size: 45px;
+    }
+
+    .header-text-container {
+      width: 100%;
+    }
+  }
+
+  @media only screen and (max-width: 500px) {
+
+    .header-text-greeting {
+      font-size: 55px;
+    }
+
+    .header-text-container {
+      width: 100%;
+    }
+
+    .header-text-centered {
+      font-size: 25px;
+    }
+  }
+</style>
---
+++ src/components/CardList.vue
@@ -0,0 +1,131 @@
+<template>
+  <div class="list-container">
+    <template v-for="(imageData, index) in imageDataList" :key="index">
+      <div class="responsive">
+        <div class="image-content">
+          <img src="https://via.placeholder.com/400x300" width="600" height="400">
+          <p class="image-content-text">Powered by HTML.COM</p>
+        </div>
+        <p class="desc">{{imageData.description}}
+        </p>
+        <button>READ MORE</button>
+      </div>
+    </template>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return{
+        imageDataList:[
+          {
+            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad menim veniam, quis nostrud exercitation
+        ullamco laboris nisi ut aliquip ex ea commodo consequat.`
+          },
+          {
+            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua.`
+          },
+          {
+            description: `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+        incididunt ut labore et dolore magna aliqua. Ut enim ad menim veniam, quis nostrud exercitation
+        ullamco.`
+          }
+        ]
+      }
+    },
+    name: 'CardList',
+    props: {
+      msg: String
+    }
+  }
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  img {
+    width: 100%;
+    height: auto;
+    padding: 1rem;
+  }
+
+  .image-content {
+      position: relative;
+  }
+
+  .image-content-text {
+    position: absolute;
+    bottom: 25px;
+    right: 20px;
+    font-size: 5px;
+    color: rgb(165, 165, 165);
+  }
+
+  .desc {
+    padding: 15px;
+    text-align: center;
+    height: 150px;
+    font-size: 15px;
+    padding-top: 0px;
+  }
+
+  * {
+    box-sizing: border-box;
+  }
+
+  .responsive {
+    float: left;
+    width: 30%;
+    border: 1px solid rgb(223, 223, 223);
+    border-radius: 5px;
+    margin: 20px;
+    padding-bottom: 10px;
+    font-size: 20px;
+  }
+
+  .list-container {
+    margin: 1rem;
+    display: flex;
+    justify-content: center;
+    margin-left: 200px;
+    margin-right: 200px;
+  }
+
+  @media only screen and (max-width: 700px) {
+    .responsive {
+      width: 49.99999%;
+      margin: 6px 0;
+    }
+
+    .list-container {
+      margin: 2rem;
+      display: block;
+    }
+
+    .desc {
+      height: unset;
+    }
+  }
+
+  @media only screen and (max-width: 500px) {
+    .responsive {
+      width: 100%;
+    }
+
+    .list-container {
+      margin: 1rem;
+      display: block;
+    }
+
+    .desc {
+      height: unset;
+      font-size: 18px;
+    }
+
+    .image-content-text {
+      font-size: 8px;
+    }
+  }
+</style>
